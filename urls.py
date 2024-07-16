afd7c6e36f9a5996b2e9604c428e0bbdb65f8150
kubaguziel <jakub.guziel@onet.pl>
2024-07-16 10:23:38 +0200

    już działa

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-#zmienna przechowująca adresy url
-urlpatterns = [
-    url('admin/',admin.site.urls),
-    url('',include('learning_logs.urls')),
-]
